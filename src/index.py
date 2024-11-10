93fef23d9a3e2544341a5874f1973af585f8a200
Matias Palmroth <matias.palmroth@gmail.com>
2024-11-10 19:57:06 +0200

    3. Tehtävä tehty. Pylint.

--- src/index.py
+++ src/index.py
@@ -1,19 +1,12 @@
 from varasto import Varasto
 
+def tulosta_varaston_tiedot(varasto_nimi, varasto):
+    print(f"{varasto_nimi} getterit:")
+    print(f"saldo = {varasto.saldo}")
+    print(f"tilavuus = {varasto.tilavuus}")
+    print(f"paljonko_mahtuu = {varasto.paljonko_mahtuu()}")
 
-def main():
-    mehua = Varasto(100.0)
-    olutta = Varasto(100.0, 20.2)
-
-    print("Luonnin jälkeen:")
-    print(f"Mehuvarasto: {mehua}")
-    print(f"Olutvarasto: {olutta}")
-
-    print("Olut getterit:")
-    print(f"saldo = {olutta.saldo}")
-    print(f"tilavuus = {olutta.tilavuus}")
-    print(f"paljonko_mahtuu = {olutta.paljonko_mahtuu()}")
-
+def kasittele_mehuvarasto(mehua):
     print("Mehu setterit:")
     print("Lisätään 50.7")
     mehua.lisaa_varastoon(50.7)
@@ -22,6 +15,7 @@ def main():
     mehua.ota_varastosta(3.14)
     print(f"Mehuvarasto: {mehua}")
 
+def tulosta_virhetilanteet():
     print("Virhetilanteita:")
     print("Varasto(-100.0);")
     huono = Varasto(-100.0)
@@ -31,28 +25,47 @@ def main():
     huono = Varasto(100.0, -50.7)
     print(huono)
 
+def kasittele_olutvarasto(olutta):
     print(f"Olutvarasto: {olutta}")
     print("olutta.lisaa_varastoon(1000.0)")
     olutta.lisaa_varastoon(1000.0)
     print(f"Olutvarasto: {olutta}")
 
+def kasittele_mehuvirhe(mehua):
     print(f"Mehuvarasto: {mehua}")
     print("mehua.lisaa_varastoon(-666.0)")
     mehua.lisaa_varastoon(-666.0)
     print(f"Mehuvarasto: {mehua}")
 
+def ota_olutta(olutta):
     print(f"Olutvarasto: {olutta}")
     print("olutta.ota_varastosta(1000.0)")
     saatiin = olutta.ota_varastosta(1000.0)
     print(f"saatiin {saatiin}")
     print(f"Olutvarasto: {olutta}")
 
+def ota_mehua(mehua):
     print(f"Mehuvarasto: {mehua}")
     print("mehua.otaVarastosta(-32.9)")
     saatiin = mehua.ota_varastosta(-32.9)
     print(f"saatiin {saatiin}")
     print(f"Mehuvarasto: {mehua}")
 
+def main():
+    mehua = Varasto(100.0)
+    olutta = Varasto(100.0, 20.2)
+
+    print("Luonnin jälkeen:")
+    print(f"Mehuvarasto: {mehua}")
+    print(f"Olutvarasto: {olutta}")
+
+    tulosta_varaston_tiedot("Olut", olutta)
+    kasittele_mehuvarasto(mehua)
+    tulosta_virhetilanteet()
+    kasittele_olutvarasto(olutta)
+    kasittele_mehuvirhe(mehua)
+    ota_olutta(olutta)
+    ota_mehua(mehua)
 
 if __name__ == "__main__":
     main()
